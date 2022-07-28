8378f6f5f412ade93b4210d9e50d3094cbd85e3f
Sergiu333 <anghelenicis59@gmail.com>
2022-07-28 16:40:10 +0300

    added header

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -12,9 +12,41 @@ export default function Navbar() {
     let { t } = useTranslation();
 
     return (
-    <div className={styles.sergiu} >
-        sss
-    </div>
+        <nav>
+            <div className="container mx-auto">
+                <ul className="nav-list">
+                    <li className="nav-logo">
+                        <div className="logo">
+                            <p>LOGO</p>
+                        </div>
+                        <button className="btn" id="nav-toggle">
+                            <i className="fa fa-bars"></i>
+                        </button>
+                    </li>
+
+                    <li className="nav-link">About<i className="fa fa-chevron-up"></i>
+                        <ul className="nav-drop">
+                            <li>Our Team</li>
+                            <li>Our Process</li>
+                            <li>History</li>
+                        </ul>
+                    </li>
+
+                    <li className="nav-link">Work<i className="fa fa-chevron-up"></i>
+                        <ul className="nav-drop">
+                            <li>Portfolio</li>
+                            <li>Showcase</li>
+                        </ul>
+                    </li>
+
+                    <li className="nav-link">Contact</li>
+                    <li className="nav-item">
+                        <button className="btn">Sign in</button>
+                    </li>
+
+                </ul>
+            </div>
+        </nav>
     );
 }
 
